3ea3da7346ac8c16d3a40fce4cec4a53c0ed000f
RazibHossain <razib@divine-it.net>
2025-04-06 16:30:35 +0600

    html added

--- hello_world/my_tennis_club/members/views.py
+++ hello_world/my_tennis_club/members/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def members(request):
-    return HttpResponse("Hello world!")
+  template = loader.get_template('myfirst.html')
+  return HttpResponse(template.render())
